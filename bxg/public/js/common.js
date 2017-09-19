98f0e43ce8a393db9d094c1b0c9f2eb5e3074612
zzy <772859254@qq.com>
2017-09-19 12:59:42 +0800

    项目V1.0.1

--- bxg/public/js/common.js
+++ bxg/public/js/common.js
@@ -6,3 +6,30 @@
 	$('.navs ul').prev('a').on('click', function () {
 		$(this).next().slideToggle();
 	});
+
+
+
+	/*实现退出功能*/
+	$('#logoutBtn').on('click',function(){
+		$.ajax({
+			type:'post',
+			url:'/api/logout',
+			dataType:'json',
+			success:function(data){
+		        if(data.code == 200){
+					location.href='/main/login'
+				}
+			}
+		})
+	});
+	//检测用户是否登陆
+	  var flag=$.cookie('PHPSESSID');
+	if(!flag){
+		location.href='/main/login'
+	}
+	//获取到用户头像信息
+    var loginInfo=$.cookie('loginInfo');
+	loginInfo=loginInfo&&JSON.parse(loginInfo);
+	//设置用户的头像信息
+	$('.aside .profile img').attr('src',loginInfo.tc_avatar);
+	$('.aside .profile h4').html(loginInfo.tc_name);
