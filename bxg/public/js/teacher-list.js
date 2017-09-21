3f4c4690147d7f5dbf930d23b549240c8753efad
zzy <772859254@qq.com>
2017-09-21 21:04:38 +0800

    完成启用和注销功能

--- bxg/public/js/teacher-list.js
+++ bxg/public/js/teacher-list.js
@@ -8,7 +8,35 @@ define(['jquery','template'],function($,template){
         dataType:'json',
         success:function(data){
              var html=template('teacherTal',{list:data.result})
-             $('#teacherInfo').html(html)
+             $('#teacherInfo').html(html);
+            //登陆注销功能
+            $('.eod').click(function(){
+                var that=$(this);
+                  //获取到父元素
+                  var td=$(this).parent();
+                  // $(this).closest('td')  找到离他最近的父元素
+                  var tcId=td.attr('data-tcId');
+                  var status=td.attr('data-status');
+                $.ajax({
+                     type:'post',
+                     url:'/api/teacher/handle',
+                     dataType:'json',
+                     data:{tc_id:tcId,tc_status:status},
+                     success:function(data){
+                         console.log(data);
+                         if(data.code == 200){
+                             td.attr('data-status',data.result.tc_status);
+                             if(data.result.tc_status == 0){
+                                 $(that).text('注销')
+                             }else{
+                                 $(that).text('开启')
+                             }
+                         }
+                     }
+                })
+            })
+
+
         }
     })
 });
