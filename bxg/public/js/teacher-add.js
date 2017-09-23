6493c50b7d58d8b656e084276a9f21e25acfe93a
zzy <772859254@qq.com>
2017-09-23 15:23:25 +0800

    添加表单验证和提交插件

--- bxg/public/js/teacher-add.js
+++ bxg/public/js/teacher-add.js
@@ -24,21 +24,52 @@ define(['jquery','template','util','datePicker','language','validate','form'],fu
         //处理表单提交
         submitForm('/api/teacher/add');
       }
-       //实现表单提交
-      function  submitForm(url){
-            $('#teacherBtn').click(function(){
-                $.ajax({
-                    type:'post',
-                    url:url,
-                    data:$('#teacherForm').serialize(),
-                    dataType:'json',
-                    success:function(data){
-                        if(data.code==200){
-                            location.href='/teacher/list';
+        // 采用表单验证和提交插件提交插件
+        function submitForm(url){
+            $('#teacherForm').validate({
+                sendForm:false,  //去掉默认的
+                valid:function(){
+                   $(this).ajaxSubmit({
+                       type:'post',
+                       url:url,
+                       dataType:'json',
+                       success:function(data){
+                       if(data.code==200){
+                       location.href='/teacher/list';
                         }
-                    }
-                })
+                       }
+                   })
+                },
+                description:{
+                     tcName:{
+                         required:'用户名不能为空'
+                     },
+                     tcPass:{
+                         required:'密码不能为空',
+                         pattern:'密码必须为六位数'
+                     },
+                     tcJoinDate:{
+                         required:'日期不能为空'
+                     }
+                }
+
             })
-      }
+        }
+       //实现表单提交
+      //function  submitForm(url){
+      //      $('#teacherBtn').click(function(){
+      //          $.ajax({
+      //              type:'post',
+      //              url:url,
+      //              data:$('#teacherForm').serialize(),
+      //              dataType:'json',
+      //              success:function(data){
+      //                  if(data.code==200){
+      //                      location.href='/teacher/list';
+      //                  }
+      //              }
+      //          })
+      //      })
+      //}
 
 });
