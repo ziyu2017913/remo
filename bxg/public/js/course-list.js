749afba9827f663cd205122fc700aff4e0fab1e4
zzy <772859254@qq.com>
2017-09-25 11:14:58 +0800

    完成课程列表

--- bxg/public/js/course-list.js
+++ bxg/public/js/course-list.js
@@ -3,4 +3,15 @@
  */
 define(['jquery','util','template'],function($,util,template){
     util.setMenu(location.pathname)
-})
+    $.ajax({
+      type:'get',
+      url:'/api/course',
+      dataType:'json',
+      success:function(data){
+      //    ‰÷»æ“≥√Ê
+          console.log(data);
+          var html=template('courseTpl',{list:data.result});
+          $('#courseInfo').html(html)
+      }
+    })
+});
